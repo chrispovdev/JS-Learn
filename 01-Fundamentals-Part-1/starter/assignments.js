0245ee07a656011005a78abddeaf896529c38240
Christian <christiantejedordev@gmail.com>
2024-09-18 09:01:19 -0500

    🎉 Initial Commit

---
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -0,0 +1,12 @@
+const country = "Panama";
+const continent = "America";
+let population = 4000000;
+console.log(
+  `I live in ${country} and its continent is ${continent}, its total population is ${population}.`
+);
+
+let isIsland = false;
+let language = "Spanish";
+console.log(isIsland, population, country, language);
+
+// const continent = "Europe"; //can't change const variable
---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,6 @@
+// let js = "amazing";
+// if (js === "amazing") {
+//   alert("JavaScript is Fun!");
+// }
+// console.log();
+// alert(40 + 8 + 23 - 10);
